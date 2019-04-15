bb6e2604d4a46a64f00863f79a83a0c3dbeaac8c
Xuan Do <doxuancntp@gmail.com>
2019-04-15 16:20:37 -0400

    Commit.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let laptopContent = document.getElementById('laptopContent');
+let mobileContent = document.getElementById('mobileContent');
+
+if (window.innerWidth <= 545) {
+
+    // laptopContent.style.visibility = "hidden";
+    // mobileContent.style.visibility = "visible";
+    // laptopContent.setAttribute("viewBox", "0 0 360 640");
+
+    laptopContent.style.display = "none";
+    mobileContent.style.display = "block";
+
+
+
+} else {
+
+    laptopContent.style.display = "block";
+    mobileContent.style.display = "none";
+
+
+}
+
+
+window.addEventListener('resize', function() {
+
+
+    if (window.innerWidth <= 545) {
+
+        laptopContent.style.display = "none";
+        mobileContent.style.display = "block";
+
+    } else {
+
+        laptopContent.style.display = "block";
+        mobileContent.style.display = "none";
+
+    }
+
+
+}, false);
