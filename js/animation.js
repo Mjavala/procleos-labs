e8043c6e3748c88f0e7086bd12e1248a413ce624
Xuan Do <doxuancntp@gmail.com>
2019-04-24 11:24:11 -0400

    Add  text entry field.

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,21 @@
-window.onload = function() {
-    textAnimation();
-};
+let fadeOutText = document.getElementById('fadeOutText');
+let rect = document.getElementById('Rectangle');
+let yes = document.getElementById('Yes');
 
 
-// Typewriter.
-function textAnimation() {
-    let text = document.getElementById('text');
-    
-}
+let textEntryField = document.getElementById('textEntryField');
+
+
+// OnClick of the button.
+rect.addEventListener('click', function() {
+
+
+    fadeOutText.classList.add('fadeOut');
+    rect.classList.add('fadeOut');
+    yes.classList.add('fadeOut');
+
+    textEntryField.style.visibility = "visible";
+
+
+
+}, false);
