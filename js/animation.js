910c2f68ac08135a6c17812071fa7fd98df99075
Xuan Do <doxuancntp@gmail.com>
2019-04-29 16:42:51 -0400

    Add more comments to codes.

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,15 @@
 let fadeOutText = document.getElementById('fadeOutText');
 let ctaBtn = document.getElementById('ctaBtn');
 
+// This is used for the onclick ctaBtn event.
+// When users click on the CTA button.
+// The CTA text fades out.
+// The CTA buttonis also faded out.
 
+// Then, the text entry field wrapper will be visible.
+// The text entry field wrapper includes the text entry field and Send button.
+
+// See fadeOut class styling inside the css/stylesheet.css
 let textEntryFieldWrapper = document.getElementById('textEntryFieldWrapper');
 // OnClick of the button.
 ctaBtn.addEventListener('click', function() {
--- js/script.js
+++ js/script.js
@@ -2,39 +2,24 @@ let laptopContent = document.getElementById('laptopContent');
 let mobileContent = document.getElementById('mobileContent');
 
 if (window.innerWidth <= 360) {
-
     // laptopContent.style.visibility = "hidden";
     // mobileContent.style.visibility = "visible";
     // laptopContent.setAttribute("viewBox", "0 0 360 640");
 
     laptopContent.style.display = "none";
     mobileContent.style.display = "block";
-
-
-
 } else {
-
     laptopContent.style.display = "block";
     mobileContent.style.display = "none";
-
-
 }
 
 
 window.addEventListener('resize', function() {
-
-
     if (window.innerWidth <= 360) {
-
         laptopContent.style.display = "none";
         mobileContent.style.display = "block";
-
     } else {
-
         laptopContent.style.display = "block";
         mobileContent.style.display = "none";
-
     }
-
-
 }, false);
