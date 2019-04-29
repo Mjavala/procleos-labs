8e3dafd2a52b8491bf669e9877c1a55d06f6ae40
Xuan Do <doxuancntp@gmail.com>
2019-04-29 10:42:05 -0400

    Finished CTA text and  text entry field for desktop and tablet screen.

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,12 @@
 let fadeOutText = document.getElementById('fadeOutText');
-let rect = document.getElementById('Rectangle');
-let yes = document.getElementById('Yes');
+let ctaBtn = document.getElementById('ctaBtn');
 
 
 let textEntryFieldWrapper = document.getElementById('textEntryFieldWrapper');
 // OnClick of the button.
-rect.addEventListener('click', function() {
+ctaBtn.addEventListener('click', function() {
     fadeOutText.classList.add('fadeOut');
-    rect.classList.add('fadeOut');
-    yes.classList.add('fadeOut');
+    ctaBtn.classList.add('fadeOut');
 
     textEntryFieldWrapper.style.visibility = "visible";
 }, false);
