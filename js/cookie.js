6adf08ab95608d10715cfbc6084bd4d9807d9b33
Xuan Do <doxuancntp@gmail.com>
2019-05-05 11:39:59 -0400

    Completed number 6.

--- js/cookie.js
+++ js/cookie.js
@@ -1,22 +1,51 @@
 let emailForm = document.getElementById('emailForm');
-console.log(emailForm);
 
 
 let fadeOutTextCookie = document.getElementById('fadeOutText');
 let ctaBtnCookie = document.getElementById('ctaBtn');
 
-// emailForm.addEventListener('submit', function(e) {
-//
-//     e.preventDefault();
-//
-//
-//     document.cookie = 'name=sent';
-//
-//
-// }, false);
-//
 
-
-function WriteCookie() {
-    document.cookie = "name=cookievalue";
+function setCookies(cname, value) {
+    document.cookie = cname + "=" + value;
 }
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+emailForm.addEventListener('submit', function() {
+    if (typeof(localStorage.sent) == "undefined") {
+        localStorage.sent = 1;
+        // document.cookie = "sent=" + localStorage.sent;
+        let value = localStorage.sent;
+        setCookies('sent', value);
+    } else {
+        // document.cookie = "sent=" + localStorage.sent;
+        localStorage.sent++
+        let value = localStorage.sent;
+        setCookies('sent', value);
+    }
+}, false);
+
+
+function textIn6Appear() {
+    if (getCookie('sent') >= 1) {
+        fadeOutTextCookie.style.display = 'none';
+        ctaBtnCookie.style.display = 'none';
+    }
+}
+window.onload = function() {
+    textIn6Appear();
+};
