dd0f2f6103929af43e8c46ba75ef4b5b180b83fd
Xuan Do <doxuancntp@gmail.com>
2019-05-17 17:38:27 -0400

    Commit.

--- js/responsive.js
+++ js/responsive.js
@@ -7,32 +7,34 @@
 function responsiveFrame() {
     let frameWrapper = document.querySelector('.frameWrapper');
     let second = document.querySelector('.second');
+    let third = document.querySelector('.third');
+
 
     let frameWrapperClientHeight = frameWrapper.clientHeight;
     let secondClientHeight = second.clientHeight;
+    let thirdClientHeight = third.clientHeight;
 
 
     // Landscape screen.
     if (frameWrapperClientHeight >= secondClientHeight) {
         let diff = secondClientHeight - frameWrapperClientHeight;
-        console.log(diff);
-        frameWrapper.style.top = diff + 66 + 'px';
 
-    } else {
-        let third = document.querySelector('.third');
-        let thirdClientHeight = third.clientHeight;
-
-        // Portrait screen.
+        // Fixed for Mobile Landscape
+        if (thirdClientHeight < 150) {
+            frameWrapper.style.top = diff + 32 + 'px';
+        } else {
+            frameWrapper.style.top = diff + 66 + 'px';
+        }
+    }
+    // Portrait screen.
+    else {
         let diff = secondClientHeight - frameWrapperClientHeight;
-
         // Fixed iPad Pro.
         if (thirdClientHeight > 450) {
             frameWrapper.style.top = diff + 80 + 'px';
         } else {
             frameWrapper.style.top = diff + 66 + 'px';
         }
-
-
     }
 }
 
